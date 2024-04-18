9393d62db4a1a8c82d1aea700e406eaa510502a5
milodevs <siliconvalleyn91@gmail.com>
2024-04-18 18:13:34 +0300

    Update BASE_URL in sitemap.ts to use process.env.BASE_URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,12 +3,12 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "BASE_URL",
+      url: process.env.BASE_URL || "http://localhost:3000",
       lastModified: new Date(),
       alternates: {
         languages: {
-            en: "BASE_URL",
-            es: "BASE_URL",
+            en: process.env.BASE_URL,
+            es: process.env.BASE_URL,
         },
       },
     },
