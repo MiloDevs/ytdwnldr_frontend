dab536809228b7828ad5e7824b74cb12340d64ee
milodevs <siliconvalleyn91@gmail.com>
2024-04-18 17:40:35 +0300

    Update metadata and add Open Graph tags in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,21 @@ const space_grotesk = Space_Grotesk({ weight: ["300", "400", "500", "600" , "700
 export const metadata: Metadata = {
   title: "YTDwnldr",
   description: "Download YouTube videos as MP4 & MP3 files.",
+  keywords: ["youtube", "download", "mp4", "mp3"],
+  manifest: "/site.webmanifest",
+  openGraph: {
+    title: "YTDwnldr",
+    description: "Download YouTube videos as MP4 & MP3 files.",
+    url: "https://ytdwnldr.vercel.app",
+    type: "website",
+    locale: "en_US",
+    images: [
+      {
+        url: "/logo.png",
+        alt: "YTDwnldr",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
