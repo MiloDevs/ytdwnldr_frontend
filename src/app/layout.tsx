7730aed49da0ab348199fff56c55635ab8de1941
milodevs <siliconvalleyn91@gmail.com>
2024-04-19 21:31:38 +0300

    Update dependencies and add @next/third-parties package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
-const space_grotesk = Space_Grotesk({ weight: ["300", "400", "500", "600" , "700"], subsets: ["latin"]});
+const space_grotesk = Space_Grotesk({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "YTDwnldr",
-  description: "Download YouTube videos as MP4 & MP3 files.",
+  description:
+    "Download YouTube videos as MP4 & MP3 files quickly and conveniently with YTDwnldr.",
   keywords: ["youtube", "download", "mp4", "mp3"],
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
   ),
   manifest: "/site.webmanifest",
   openGraph: {
-    title: "YTDwnldr",
-    description: "Download YouTube videos as MP4 & MP3 files quickly and conveniently with YTDwnldr.",
+    title: "YTDwnldr - YouTube Video Downloader",
+    description:
+      "YTDwnldr is a fast and easy-to-use YouTube video downloader that allows you to download videos in MP4 and MP3 formats.",
     url: "https://ytdwnldr.vercel.app",
     type: "website",
     locale: "en_US",
@@ -25,6 +30,7 @@ export const metadata: Metadata = {
         alt: "YTDwnldr",
       },
     ],
+    siteName: "YTDwnldr", // Updated site_name property
   },
   twitter: {
     site: "@YTDwnldr",
@@ -33,9 +39,14 @@ export const metadata: Metadata = {
       {
         url: "/logo.png",
         alt: "YTDwnldr",
-      }
-    ]
-  }
+      },
+    ],
+  },
+  authors: [
+    {
+      name: "milodevs",
+    },
+  ],
 };
 
 export default function RootLayout({
